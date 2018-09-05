95bdc7471f546e05a31fbe698b2e7fc57ca96009
akrasnov96 <akrasnov96@yandex-team.ru>
2018-09-05 22:40:44 +0300

    remove trash

--- tests/structures/stack.test.ts
+++ tests/structures/stack.test.ts
@@ -4,38 +4,38 @@ const stack: Stack = new Stack();
 
 describe('stack', () => {
 
-    it('stack should be empty', async () => {
+    it('stack should be empty', () => {
         expect(stack.isEmpty()).toStrictEqual(true);
     });
 
-    it('peek should return nothing while stack is empty', async () => {
+    it('peek should return nothing while stack is empty', () => {
         expect(stack.peek()).toStrictEqual(undefined);
     });
 
-    it('remove should return nothing while stack is empty', async () => {
+    it('remove should return nothing while stack is empty', () => {
         expect(stack.remove()).toStrictEqual(undefined);
     });
 
-    it('should add element to stack', async () => {
+    it('should add element to stack', () => {
         stack.add('foo');
         expect(stack.peek()).toStrictEqual('foo');
     });
 
-    it('should add one more element', async () => {
+    it('should add one more element', () => {
         stack.add('bar');
         expect(stack.peek()).toStrictEqual('bar');
     });
 
-    it('should remove element from top', async () => {
+    it('should remove element from top', () => {
         expect(stack.remove()).toStrictEqual('bar');
         expect(stack.peek()).toStrictEqual('foo');
     });
 
-    it('stack should be not empty', async () => {
+    it('stack should be not empty', () => {
         expect(stack.isEmpty()).toStrictEqual(false);
     });
 
-    it('should remove last element from stack', async () => {
+    it('should remove last element from stack', () => {
         expect(stack.remove()).toStrictEqual('foo');
         expect(stack.peek()).toStrictEqual(undefined);
         expect(stack.isEmpty()).toStrictEqual(true);
