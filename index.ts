9582f23a8c724f2f728c6f333914eefa230e2348
akrasnov96 <akrasnov96@yandex-team.ru>
2018-09-05 23:35:00 +0300

    add simple readme

---
+++ index.ts
@@ -0,0 +1,17 @@
+import {Collection} from './src/structures/collection';
+import {Stack} from './src/structures/array-based/stack';
+import {OrderedArray} from './src/structures/array-based/ordered-array';
+import {Deque} from './src/structures/array-based/queues/deque';
+import {Queue} from './src/structures/array-based/queues/queue';
+import {CircularQueue} from './src/structures/array-based/queues/queue-cycle';
+import {PriorityQueue} from './src/structures/array-based/queues/queue-priority';
+
+export {
+    Collection,
+    Stack,
+    OrderedArray,
+    Deque,
+    Queue,
+    CircularQueue,
+    PriorityQueue,
+};
---
+++ readme.md
@@ -0,0 +1,9 @@
+[![Build Status](https://travis-ci.org/aikrasnov/typescript-collections.svg?branch=master)](https://travis-ci.org/aikrasnov/typescript-collections)
+
+Now available: 
+* stack
+* ordered array
+* queue
+* circular queue
+* priority queue
+* deque
