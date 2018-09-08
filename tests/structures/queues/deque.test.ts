96369700216cadaef07d88e076dbafeb131d1e1c
akrasnov96 <akrasnov96@yandex-team.ru>
2018-09-08 20:34:27 +0300

    remove trash

--- tests/structures/queues/deque.test.ts
+++ tests/structures/queues/deque.test.ts
@@ -4,46 +4,46 @@ const deque: Deque<any> = new Deque();
 
 describe('deque', () => {
 
-    it('should be empty', async () => {
+    it('should be empty', () => {
         expect(deque.isEmpty()).toStrictEqual(true);
     });
 
-    it('remove should return undefined', async () => {
+    it('remove should return undefined', () => {
         expect(deque.remove()).toStrictEqual(undefined);
     });
 
-    it('removeFromFront should return undefined', async () => {
+    it('removeFromFront should return undefined', () => {
         expect(deque.removeFromFront()).toStrictEqual(undefined);
     });
 
-    it('peek should return undefined', async () => {
+    it('peek should return undefined', () => {
         expect(deque.peek()).toStrictEqual(undefined);
     });
 
-    it('peekFront should return undefined', async () => {
+    it('peekFront should return undefined', () => {
         expect(deque.peekFront()).toStrictEqual(undefined);
     });
 
-    it('should add element to queue', async () => {
+    it('should add element to queue', () => {
         deque.add(1);
         expect(deque.peek()).toStrictEqual(1);
     });
 
-    it('should be not empty', async () => {
+    it('should be not empty', () => {
         expect(deque.isEmpty()).toStrictEqual(false);
     });
 
-    it('should add element to begin queue', async () => {
+    it('should add element to begin queue', () => {
         deque.add(2);
         expect(deque.peekFront()).toStrictEqual(2);
     });
 
-    it('should add element to end queue', async () => {
+    it('should add element to end queue', () => {
         deque.addToBack(3);
         expect(deque.peek()).toStrictEqual(3);
     });
 
-    it('should add more element to queue', async () => {
+    it('should add more element to queue', () => {
         deque.add(4);
         deque.addToBack(5);
         deque.add(6);
@@ -53,12 +53,12 @@ describe('deque', () => {
         expect(deque.peekFront()).toStrictEqual(6);
     });
 
-    it('should remove element from front', async () => {
+    it('should remove element from front', () => {
         expect(deque.removeFromFront()).toStrictEqual(6);
         expect(deque.peekFront()).toStrictEqual(4);
     });
 
-    it('should remove element from back', async () => {
+    it('should remove element from back', () => {
         expect(deque.remove()).toStrictEqual(7);
         expect(deque.peek()).toStrictEqual(5);
     });
